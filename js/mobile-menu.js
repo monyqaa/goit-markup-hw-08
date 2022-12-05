d02a458a6e7c8ce341656116b43ce2f81771ced8
Monika Gołuchowska <moniagoluchowska@gmail.com>
2022-12-05 02:37:31 +0100

    Responsywność cz. 2

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,5 +1,5 @@
 const menuOverlay = document.querySelector(".mobile-menu");
-const closeMenuButton = document.querySelector(".toggle-menu");
+const closeMenuButton = document.querySelector(".mobile-menu__close-btn");
 const openMenuBtn = document.querySelector(".header__mobile-menu-open-btn");
 
 openMenuBtn.addEventListener("click", (e) => {
